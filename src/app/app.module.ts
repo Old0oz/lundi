f505eb7db56b6e32430f42f34dddfc10c700123a
Unknown <vivien.iob@gmail.com>
2018-09-24 13:44:43 +0200

    Creation des composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalendrierComponent } from './composants/calendrier/calendrier/calendrier.component';
+import { FormulaireComponent } from './composants/formulaire/formulaire.component';
+import { Formulaire2Component } from './composants/formulaire2/formulaire2.component';
+import { CoursesComponent } from './composants/courses/courses.component';
+import { LoginComponent } from './composants/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendrierComponent,
+    FormulaireComponent,
+    Formulaire2Component,
+    CoursesComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
