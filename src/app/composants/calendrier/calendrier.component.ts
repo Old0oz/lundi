72e471d97f31abf2b7069deae56d058c6860e42e
Mili92 <milirogbeer@hotmail.com>
2018-09-26 16:33:26 +0200

    calendarcommit

--- src/app/composants/calendrier/calendrier.component.ts
+++ src/app/composants/calendrier/calendrier.component.ts
@@ -1,15 +1,131 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import * as moment from 'moment';
+import * as _ from 'lodash';
+import { JsonPipe } from '../../../../node_modules/@angular/common';
 
+export interface CalendarDate {
+  mDate: moment.Moment;
+  selected?: boolean;
+  today?: boolean;
+}
 @Component({
-  selector: 'app-calendrier',
+  selector: 'app-calendar',
   templateUrl: './calendrier.component.html',
-  styleUrls: ['./calendrier.component.css']
+  styleUrls: ['./calendrier.component.scss']
 })
-export class CalendrierComponent implements OnInit {
+export class CalendrierComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  currentDate = moment();
+  dayNames = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+  weeks: CalendarDate[][] = [];
+  sortedDates: CalendarDate[] = [];
 
-  ngOnInit() {
+  @Input() selectedDates: CalendarDate[] = [];
+  @Output() onSelectDate = new EventEmitter<CalendarDate>();
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.generateCalendar();
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.selectedDates &&
+        changes.selectedDates.currentValue &&
+        changes.selectedDates.currentValue.length  > 1) {
+      // sort on date changes for better performance when range checking
+      this.sortedDates = _.sortBy(changes.selectedDates.currentValue, (m: CalendarDate) => m.mDate.valueOf());
+      this.generateCalendar();
+    }
+  }
+
+  // date checkers
+  isToday(date: moment.Moment): boolean {
+    return moment().isSame(moment(date), 'day');
+  }
+
+  isSelected(date: moment.Moment): boolean {
+    return _.findIndex(this.selectedDates, (selectedDate) => {
+      return moment(date).isSame(selectedDate.mDate, 'day');
+    }) > -1;
+  }
+
+  isSelectedMonth(date: moment.Moment): boolean {
+    return moment(date).isSame(this.currentDate, 'month');
+  }
+
+  selectDate(date: CalendarDate): void {
+    this.onSelectDate.emit(date);
+    let DATE: string;
+    DATE = JSON.stringify(date.mDate).substr(1,10);
+    let DATE2=moment(DATE,'YYYY-MM-DD');
+    DATE2=DATE2.add(1,'day');
+    this.noConnexion=!this.noConnexion;
+    console.log(DATE);
+    console.log(DATE2);
+  }
+
+  // actions from calendar
+  prevMonth(): void {
+    this.currentDate = moment(this.currentDate).subtract(1, 'months');
+    this.generateCalendar();
+  }
+
+  nextMonth(): void {
+    this.currentDate = moment(this.currentDate).add(1, 'months');
+    this.generateCalendar();
+  }
+
+  firstMonth(): void {
+    this.currentDate = moment(this.currentDate).startOf('year');
+    this.generateCalendar();
+  }
+
+  lastMonth(): void {
+    this.currentDate = moment(this.currentDate).endOf('year');
+    this.generateCalendar();
+  }
+
+  prevYear(): void {
+    this.currentDate = moment(this.currentDate).subtract(1, 'year');
+    this.generateCalendar();
+  }
+
+  nextYear(): void {
+    this.currentDate = moment(this.currentDate).add(1, 'year');
+    this.generateCalendar();
+  }
+
+  // generate the calendar grid
+  generateCalendar(): void {
+    const dates = this.fillDates(this.currentDate);
+    const weeks: CalendarDate[][] = [];
+    while (dates.length > 0) {
+      weeks.push(dates.splice(0, 7));
+      
+    }
+    this.weeks = weeks;
+  }
+
+  fillDates(currentMoment: moment.Moment): CalendarDate[] {
+    const firstOfMonth = moment(currentMoment).startOf('month').day();
+    const firstDayOfGrid = moment(currentMoment).startOf('month').subtract(firstOfMonth, 'days');
+    const start = firstDayOfGrid.date();
+    return _.range(start, start + 42)
+            .map((date: number): CalendarDate => {
+              const d = moment(firstDayOfGrid).date(date);
+              return {
+                today: this.isToday(d),
+                selected: this.isSelected(d),
+                mDate: d,
+              };
+            });
+  }
+
+  public noConnexion: Boolean = true;
+  public toggleConnexion(): void {
+    this.noConnexion=!this.noConnexion;
+  }
+  
 
 }
